721ab9ab93559c3ec878e6c2180e184243bac80e
Nick <nicholaschamboko@gmail.com>
2024-02-11 14:27:21 +0200

    working

--- Luhn Algorith/luhn.py
+++ Luhn Algorith/luhn.py
@@ -1,1 +1,13 @@
+"""
+    This algorithm is used to validate a variety of identification numbers
+"""
+def main():
+    card_number = '4111-1111-4555-1142'
 
+#maketrans method replaces characters in a string
+nick = 'ticholas'
+
+print(nick)
+trs = str.maketrans({'t':'c','l':'b'})
+translated_string = nick.translate(trs)
+print(translated_string)
