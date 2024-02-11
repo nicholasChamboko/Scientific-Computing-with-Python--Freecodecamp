e82967e2b6f93f43e23e164b1b9ad71ec38b8f87
Nick <nicholaschamboko@gmail.com>
2024-02-11 13:55:32 +0200

    Full Cipher

--- Cipher/main.py
+++ Cipher/main.py
@@ -1,6 +1,31 @@
-message = 'Hello World'
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+text = 'mrttaqrhhknsw ih puggrur'
+custom_key = 'python'
 
-def vigenere(message, offset):
-    pass
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyx'
+    final_message = ''
+
+    for char in message.lower():
+
+        #Append a non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:
+            #Find right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            #Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset * direction) % len(alphabet)
+            final_message += alphabet[new_index]
+
+    return final_message
+def encrypt(message, key):
+    return vigenere(message, key)
+
+def decrypt(message, key):
+    return vigenere(message, key, -1)
 
