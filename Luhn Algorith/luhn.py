3b8bc34bc4e79ad4a1fde29c63440e67a116c4bb
Nick <nicholaschamboko@gmail.com>
2024-02-11 14:52:12 +0200

    translation

--- Luhn Algorith/luhn.py
+++ Luhn Algorith/luhn.py
@@ -4,10 +4,12 @@
 def main():
     card_number = '4111-1111-4555-1142'
 
-#maketrans method replaces characters in a string
-nick = 'ticholas'
+    #maketrans method replaces characters in a string eg. - will be replaced with nospace and space will be replaced with no space as well
+    card_translation = str.maketrans({'-' : '', ' ':''})
 
-print(nick)
-trs = str.maketrans({'t':'c','l':'b'})
-translated_string = nick.translate(trs)
-print(translated_string)
+    #translating the card number using the specified translation
+    translated_card_number  = card_number.translate(card_translation)
+
+    print(translated_card_number)
+
+main()
