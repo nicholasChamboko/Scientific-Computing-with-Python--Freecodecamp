c7ca23ba0c74afb60fc03af7eb754a2ca0fe6325
Nick <nicholaschamboko@gmail.com>
2024-02-11 21:55:57 +0200

    finalized code

--- Luhn Algorith/luhn.py
+++ Luhn Algorith/luhn.py
@@ -1,6 +1,7 @@
 """
     This algorithm is used to validate a variety of identification numbers
 """
+
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
 
@@ -13,7 +14,18 @@ def verify_card_number(card_number):
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
     
-    print(sum_of_odd_digits)
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+        sum_of_even_digits += number
+
+    total = sum_of_even_digits + sum_of_odd_digits
+    print(total)
+    return total % 10 == 0
 
 def main():
     card_number = '4111-1111-4555-1142'
@@ -24,6 +36,9 @@ def main():
     #translating the card number using the specified translation
     translated_card_number  = card_number.translate(card_translation)
 
-    verify_card_number(translated_card_number)
+    if verify_card_number(translated_card_number):
+        print('Valid Card')
+    else:
+        print('Invalid Card')
 
 main()
