5ea1d1edc420480adf211fcf73a9b9a521094d94
Nick <nicholaschamboko@gmail.com>
2024-02-21 21:00:02 +0200

    .

---
+++ Shortest Path Algorithm/main.py
@@ -0,0 +1,18 @@
+"""
+    Learning algorithm design by building a shortest path first algorithm
+"""
+
+copper = {
+    'species' : 'guinea pig',
+    'age' : 2,
+
+}
+
+#Acceessing the data in the dictionary by key
+print(copper['species'])
+
+#Assigning a new key,value pair to the dictionary
+copper['colour'] = 'brown'
+#Changing the value of a key in the dictionary
+copper['species'] = 'Rhodesian Ridgeback'
+print(copper)
